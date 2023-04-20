1e351b5030c54cabadf54d964622d54551ce7327
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-20 11:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-const inputs = document.querySelectorAll('.controls input');
+  const video = document.querySelector('.flex');
+  const speed = document.querySelector('.speed');
+  const speedBar = document.querySelector('.speed-bar');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  function handlePlay() {
+    const playButton = document.querySelector('.play-button');
+    if (video.paused) {
+      video.play();
+      playButton.textContent = '❚❚';
+    } else {
+      video.pause();
+      playButton.textContent = '►';
     }
+  }
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+  function handleSpeedUpdate() {
+    video.playbackRate = parseFloat(this.value);
+    speedBar.textContent = `${video.playbackRate.toFixed(2)}×`;
+  }
+
+  function handleSkip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+  }
+
+  function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    const progressBar = document.querySelector('.progress-filled');
+    progressBar.style.flexBasis = `${percent}%`;
+  }
+
+  function scrub(e) {
+    const scrubTime = (e.offsetX / speed.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+  }
+
+  video.addEventListener('click', handlePlay);
+  video.addEventListener('timeupdate', handleProgress);
+
+  const playButton = document.querySelector('.play-button');
+  playButton.addEventListener('click', handlePlay);
+
+  const skipButtons = document.querySelectorAll('[data-skip]');
+  skipButtons.forEach(button => button.addEventListener('click', handleSkip));
+
+  const playbackSpeed = document.querySelector('.playback-speed');
+  playbackSpeed.addEventListener('input', handleSpeedUpdate);
+
+  let isScrubbing = false;
+  speed.addEventListener('mousedown', () => isScrubbing = true);
+  speed.addEventListener('mousemove', (e) => isScrubbing && scrub(e));
+  speed.addEventListener('mouseup', () => isScrubbing = false);
+  speed.addEventListener('mouseleave', () => isScrubbing = false);
